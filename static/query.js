e22c67d7f6c33b55fa67cd437512a0f0562661f0
oerraoui <itsscarl2022@gmail.com>
2025-11-08 05:38:14 +0100

    Add user query function to fetch user details and transactions

---
+++ static/query.js
@@ -0,0 +1,29 @@
+export function query() {
+  return `
+{
+  user {
+    id
+    login
+    email
+    lastName
+    firstName
+    avatarUrl
+    auditRatio
+    campus
+    xps {
+      amount
+      path
+    }
+    transactions(
+      order_by: [{ type: desc }, { amount: desc }]
+      distinct_on: [type]
+      where: {
+        type: { _like: "skill_%" }
+      }
+    ) {
+      type
+      amount
+    }
+  }
+}
+`};
