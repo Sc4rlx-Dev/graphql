688c7bbbc6db453abb32c1879773e6a2b3229a6e
oerraoui <itsscarl2022@gmail.com>
2025-11-08 05:17:20 +0100

    query function for improved readability and maintainability

--- src/js/query.js
+++ src/js/query.js
@@ -1,1 +1,30 @@
-//q
+export function query() {
+  return `
+{
+  user {
+    id
+    login
+    email
+    lastName
+    firstName
+    avatarUrl
+    auditRatio
+    campus
+    xps {
+      amount
+      path
+    }
+    transactions(
+      order_by: [{ type: desc }, { amount: desc }]
+      distinct_on: [type]
+      where: {
+        type: { _like: "skill_%" }
+      }
+    ) {
+      type
+      amount
+    }
+  }
+}
+`;
+}
